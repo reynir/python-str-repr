9413b54aaaa7a17b451f27fbc10d2e983898d88a
Reynir Björnsson <reynir@reynir.dk>
2024-01-31 14:04:38 +0100

    py test: test against python unicode version
    The Unicode version compiled with Python may very well be older than the
    one compiled with uucp. To prevent errors due to mismatch in unicode
    version we get the unicode version from Python and test against that.

--- test/test_py_python_str_repr.ml
+++ test/test_py_python_str_repr.ml
@@ -1,8 +1,18 @@
+let py_unicode_version =
+  let open Py in
+  let unicodedata = PyModule.import "unicodedata" in
+  let unicode_version = unicodedata $. String "unidata_version" in
+  let unicode_version = Object.to_string unicode_version in
+  match String.split_on_char '.' unicode_version with
+  | major :: minor :: _ -> (int_of_string major, int_of_string minor)
+  | _ -> assert false
 
 let py_repr =
   let open Py in
   let py_repr_fn = Object.get_item_s (builtins ()) "str" $. String "__repr__" in
   PyWrap.(python_fn (string @-> returning string)) py_repr_fn
+
+let repr = Python_str_repr.repr ~unicode_version:py_unicode_version
 
 let test_repr =
   let inputs = [
@@ -15,7 +25,7 @@ let test_repr =
   in
   List.map (fun input ->
       Printf.sprintf "%S" input, `Quick, (fun () ->
-          Alcotest.(check string) input (py_repr input) (Python_str_repr.repr input)))
+          Alcotest.(check string) input (py_repr input) (repr input)))
     inputs
 
 let test_repr_unicode =
@@ -25,7 +35,7 @@ let test_repr_unicode =
   in
   List.map (fun input ->
       Printf.sprintf "%S" input, `Quick, (fun () ->
-          Alcotest.(check string) input (py_repr input) (Python_str_repr.repr input)))
+          Alcotest.(check string) input (py_repr input) (repr input)))
     inputs
 
 let test_repr_qcheck =
@@ -40,12 +50,12 @@ let test_repr_qcheck =
     QCheck2.Test.make
       ~print:(fun s ->
           Printf.sprintf "%S: %S vs %S" s
-            (py_repr s) (Python_str_repr.repr s))
+            (py_repr s) (repr s))
       ~name:"repr qcheck"
       QCheck2.Gen.string
       (fun s ->
          QCheck2.assume (valid_utf8 s);
-         String.equal (py_repr s) (Python_str_repr.repr s))
+         String.equal (py_repr s) (repr s))
   in
   [ QCheck_alcotest.to_alcotest test_case ]
 
@@ -56,6 +66,8 @@ let test_suites = [
 ]
 
 let () =
+  Printf.printf "Using Unicode version %d.%d\n"
+    (fst py_unicode_version) (snd py_unicode_version);
   Alcotest.run
     "python-str-repr"
     test_suites
